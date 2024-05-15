14083ba0d6eef0e742c24b65b22c28537d0e60d5
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-05-16 00:14:17 +0200

    Optimize jpeg

--- optimize.sh
+++ optimize.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-find "./data/24" -type f -name "*.png" -print | while read file
-do
+find "./data/24" -type f -name "*.png" -print | while read file; do
    cwebp -quiet -q 80 "$file" -o "${file%.*}.op.webp"
    cwebp -quiet -q 80 -resize 150 0 "$file" -o "${file%.*}-150px.op.webp"
    cwebp -quiet -q 80 -resize 300 0 "$file" -o "${file%.*}-300px.op.webp"
    cwebp -quiet -q 80 -resize 500 0 "$file" -o "${file%.*}-500px.op.webp"
 done
 
-find "./data/24" -type f -name "*.jpg" -print | while read file
-do
+find "./data/24" -type f -name "*.jpg" -print | while read file; do
+   cwebp -quiet -q 80 "$file" -o "${file%.*}.op.webp"
+   cwebp -quiet -q 80 -resize 150 0 "$file" -o "${file%.*}-150px.op.webp"
+   cwebp -quiet -q 80 -resize 300 0 "$file" -o "${file%.*}-300px.op.webp"
+   cwebp -quiet -q 80 -resize 500 0 "$file" -o "${file%.*}-500px.op.webp"
+done
+
+find "./data/24" -type f -name "*.jpeg" -print | while read file; do
    cwebp -quiet -q 80 "$file" -o "${file%.*}.op.webp"
    cwebp -quiet -q 80 -resize 150 0 "$file" -o "${file%.*}-150px.op.webp"
    cwebp -quiet -q 80 -resize 300 0 "$file" -o "${file%.*}-300px.op.webp"
